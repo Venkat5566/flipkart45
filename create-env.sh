7489d7b9ee9043354c197c637daecaf709b5d063
JYOTHIKA <derangula666@gmail.com>
2018-11-16 11:52:16 +0530

    added dockerfile and create_env script

---
+++ create-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+count=$1
+echo "creating $count containers..."
+
+for i in `seq $count`
+do
+	echo "==============================================="
+	echo "creating flipkart$i container..."
+	docker run -it -d --name flipkart$i jyo/flipkart_img /bin/bash
+	echo "flipkart$i container has been created"
+	echo "==============================================="
+
+done
+
+docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -a -q` > IPs.txt
+
